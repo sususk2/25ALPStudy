b79721a7a76645aa0cf71759b6c3df162c43b379
sususk2 <sususk2@naver.com>
2025-03-28 14:19:53 +0900

    Create BOJ_10845.cpp

---
+++ 2025-1/Basic/sususk2/BOJ_10845.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<queue>
+#include<string>
+using namespace std;
+
+int main()
+{
+	
+	int n;
+	cin >> n;
+	string s;
+	int x;
+	queue<int> q;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> s;
+		if (s == "push")
+		{
+			cin >> x;
+			q.push(x);
+
+		}
+		else if (s == "pop")
+		{
+			if (q.empty())
+			{
+				cout << "-1" << "\n";
+			}
+			else
+			{
+
+				cout << q.front() << "\n";
+				q.pop();
+			}
+
+		}
+		else if (s == "size")
+		{
+			cout << q.size() << "\n";
+
+		}
+		else if (s == "empty")
+		{
+			if (q.empty())
+			{
+				cout << "1" << "\n";
+
+			}
+			else cout << "0" << "\n";
+		}
+		else if (s == "front")
+		{
+			if (q.empty())
+			{
+				cout << "-1" << "\n";
+
+			}
+			else cout << q.front() << "\n";
+		}
+		else if (s == "back")
+		{
+
+			if (q.empty())
+			{
+				cout << "-1" << "\n";
+
+			}
+			else cout <<q.back() << "\n";
+		}
+
+
+
+	}
+
+	
+}
